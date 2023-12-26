5e54e3b7b59f78e2d8d0ba9ec5de0b9d2f53bef7
lxj <jk01@vip.qq.com>
2023-12-26 10:03:13 +0800

    Quartz sync: Dec 26, 2023, 10:03 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 新聞深度閱讀",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
